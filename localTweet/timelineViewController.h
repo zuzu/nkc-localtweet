aee4cc5776db611f45f86878833a493a86120c33
hourai <xthstyle@hotmail.co.jp>
2012-05-25 11:54:39 +0900

    timelineViewControllerを追加

---
+++ localTweet/timelineViewController.h
@@ -0,0 +1,13 @@
+//
+//  timelineViewController.h
+//  localTweet
+//
+//  Created by CT4 on 12/05/25.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface timelineViewController : UITableViewController
+
+@end
